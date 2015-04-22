fc60a89267876d501c18229161176548d1bbe48a
daniel-hinchliff <danmath01@yahoo.com.ar>
2015-04-22 21:25:40 +0100

    Added Logger class

---
+++ Base/Logger.php
@@ -0,0 +1,23 @@
+<?php
+
+class Logger
+{
+    protected $directory;
+
+    public function __construct($log_directory)
+    {
+        $this->directory = $log_directory;
+    }
+
+    public function log($url, $content)
+    {
+        $filename = date('Y-m-d-H-i-s-') . $this->sanitizeFilename($url);
+
+        file_put_contents("$this->directory/$filename.html", $content);
+    }
+
+    protected function sanitizeFilename($filename)
+    {
+        return preg_replace('/[^a-zA-Z0-9-_\.]/','', $filename);
+    }
+}
