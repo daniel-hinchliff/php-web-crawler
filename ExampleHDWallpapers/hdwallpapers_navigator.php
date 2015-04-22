5762a44171493b2b58972371b7bac63a033fcd7b
daniel-hinchliff <danmath01@yahoo.com.ar>
2015-04-22 21:57:24 +0100

    Removed excesive fluff in HD navigator

--- ExampleHDWallpapers/hdwallpapers_navigator.php
+++ ExampleHDWallpapers/hdwallpapers_navigator.php
@@ -2,18 +2,9 @@
 
 class HDWallpapersNavigator extends Navigator
 {
-    public function isListingPage($url)
-    {
-        return preg_match(ListingPagePattern, $url);
-    }
-
-    public function isWallpaperPage($url)
-    {
-        return preg_match(WallpaperPagePattern, $url);
-    }
-
     public function test(&$url, $current_url)
     {
-        return $this->isListingPage($current_url) && $this->isWallpaperPage($url);
+        return preg_match(WallpaperPagePattern, $url)
+            && preg_match(ListingPagePattern, $current_url);
     }
 }
