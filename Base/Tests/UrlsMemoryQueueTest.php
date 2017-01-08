e637db9b5d2c704f6bf5c16405b4514a383d524b
daniel-hinchliff <danmath01@yahoo.com.ar>
2017-01-08 17:55:20 +0000

    UrlsMemoryQueueTest::testQueueOrder()

--- Base/Tests/UrlsMemoryQueueTest.php
+++ Base/Tests/UrlsMemoryQueueTest.php
@@ -10,4 +10,18 @@ class UrlsMemoryQueueTest extends PHPUnit_Framework_TestCase
 
         $this->assertNull($queue->getUrl());
     }
+
+    public function testQueueOrder()
+    {
+        $queue = new UrlsMemoryQueue();
+
+        $queue->addUrl('url 1');
+        $queue->addUrl('url 2');
+        $queue->addUrl('url 3');
+
+        $this->assertEquals('url 3', $queue->getUrl());
+        $this->assertEquals('url 2', $queue->getUrl());
+        $this->assertEquals('url 1', $queue->getUrl());
+        $this->assertNull($queue->getUrl());
+    }
 }
