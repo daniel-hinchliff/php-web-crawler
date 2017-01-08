879bd339bbd395be20c54b68d6461a0d11db0151
daniel-hinchliff <danmath01@yahoo.com.ar>
2017-01-08 21:11:12 +0000

    UrlExtractorTest::testMultipleLinks()

--- Base/Tests/UrlExtractorTest.php
+++ Base/Tests/UrlExtractorTest.php
@@ -7,11 +7,19 @@ class UrlExtractorTest extends PHPUnit_Framework_TestCase
     const urlRoot = 'http://url.com';
     const urlNotRoot = 'http://url.com/path';
     const noLinksContent = 'This is some text';
+    const multipleLinksContent = '<a href="a">a</a> <a href="b">b</a>';
 
     public function testNoLinks()
     {
         $extractor = new UrlExtractor();
         $urls = $extractor->extract(self::noLinksContent, self::urlRoot);
         $this->assertEmpty($urls);
+    }
+
+    public function testMultipleLinks()
+    {
+        $extractor = new UrlExtractor();
+        $urls = $extractor->extract(self::multipleLinksContent, self::urlRoot);
+        $this->assertEquals(2, count($urls));
     }
 }
