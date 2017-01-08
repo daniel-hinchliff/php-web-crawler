21657b76b0f8daea51a571353daecf8868cd29c5
daniel-hinchliff <danmath01@yahoo.com.ar>
2017-01-08 21:55:33 +0000

    Fix nomenclature in UrlExtractorTest

--- Base/Tests/UrlExtractorTest.php
+++ Base/Tests/UrlExtractorTest.php
@@ -9,8 +9,8 @@ class UrlExtractorTest extends PHPUnit_Framework_TestCase
     const noLinksContent = 'This is some text';
     const multipleLinksContent = '<a href="a">a</a> <a href="b">b</a>';
     const repeatedLinksContent = '<a href="a">a</a> <a href="a">b</a>';
-    const absoluteLinksContent = '<a href="/web/page">a</a>';
     const relativeLinksContent = '<a href="web/page">a</a>';
+    const rootRelativeLinksContent = '<a href="/web/page">a</a>';
 
     public function testNoLinks()
     {
@@ -33,17 +33,17 @@ class UrlExtractorTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(1, count($urls));
     }
 
-    public function testAbsoluteLinksAtRoot()
+    public function testRootRelativeLinksAtRoot()
     {
         $extractor = new UrlExtractor();
-        $urls = $extractor->extract(self::absoluteLinksContent, self::urlRoot);
+        $urls = $extractor->extract(self::rootRelativeLinksContent, self::urlRoot);
         $this->assertEquals(['http://url.com/web/page'], $urls);
     }
 
-    public function testAbsoluteLinksNotAtRoot()
+    public function testRootRelativeLinksNotAtRoot()
     {
         $extractor = new UrlExtractor();
-        $urls = $extractor->extract(self::absoluteLinksContent, self::urlNotRoot);
+        $urls = $extractor->extract(self::rootRelativeLinksContent, self::urlNotRoot);
         $this->assertEquals(['http://url.com/web/page'], $urls);
     }
 
