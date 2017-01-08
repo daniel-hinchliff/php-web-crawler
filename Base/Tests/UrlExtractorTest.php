90180a949e3a350fb8a7ad0496a44d7448832572
daniel-hinchliff <danmath01@yahoo.com.ar>
2017-01-08 21:21:34 +0000

    UrlExtractorTest fix absolute url link tests

--- Base/Tests/UrlExtractorTest.php
+++ Base/Tests/UrlExtractorTest.php
@@ -9,7 +9,7 @@ class UrlExtractorTest extends PHPUnit_Framework_TestCase
     const noLinksContent = 'This is some text';
     const multipleLinksContent = '<a href="a">a</a> <a href="b">b</a>';
     const repeatedLinksContent = '<a href="a">a</a> <a href="a">b</a>';
-    const absoluteLinksContent = '<a href="http://web/page">a</a>';
+    const absoluteLinksContent = '<a href="/web/page">a</a>';
     const relativeLinksContent = '<a href="web/page">a</a>';
 
     public function testNoLinks()
@@ -33,11 +33,18 @@ class UrlExtractorTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(1, count($urls));
     }
 
-    public function testAbsoluteLinks()
+    public function testAbsoluteLinksAtRoot()
     {
         $extractor = new UrlExtractor();
         $urls = $extractor->extract(self::absoluteLinksContent, self::urlRoot);
-        $this->assertEquals(['http://web/page'], $urls);
+        $this->assertEquals(['http://url.com/web/page'], $urls);
+    }
+
+    public function testAbsoluteLinksNotAtRoot()
+    {
+        $extractor = new UrlExtractor();
+        $urls = $extractor->extract(self::absoluteLinksContent, self::urlNotRoot);
+        $this->assertEquals(['http://url.com/web/page'], $urls);
     }
 
     public function testRelativeLinks()
