4bfbd52a4b248118f1d0f7b06f4c2c4570e973d8
daniel-hinchliff <danmath01@yahoo.com.ar>
2017-01-08 19:15:08 +0000

    ProcessorFilterTest::testProcessNotInvokedIfPageFiltered()

--- Base/Tests/ProcessorFilterTest.php
+++ Base/Tests/ProcessorFilterTest.php
@@ -26,6 +26,23 @@ class ProcessorFilterTest extends PHPUnit_Framework_TestCase
         $crawler->crawl(0);
     }
 
+    public function testProcessNotInvokedIfPageFiltered()
+    {
+        $queue = new UrlsMemoryQueue([self::url]);
+        $processor = $this->prophet->prophesize('Crawler\Processor');
+        $fetcher = $this->prophet->prophesize('Crawler\Fetcher');
+
+        $fetcher->fetch(self::url)->willReturn('content');
+        $processor->filter(self::url)->willReturn(false);
+        $processor->process('content', self::url)->shouldNotBeCalled();
+
+        $crawler = new Crawler();
+        $crawler->setProcessor($processor->reveal());
+        $crawler->setFetcher($fetcher->reveal());
+        $crawler->setQueue($queue);
+        $crawler->crawl(0);
+    }
+
     protected function setup()
     {
         $this->prophet = new \Prophecy\Prophet;
