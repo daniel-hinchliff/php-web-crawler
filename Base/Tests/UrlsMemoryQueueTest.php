4c039a9fc8d4ce075e8ee687111e3322112b4326
daniel-hinchliff <danmath01@yahoo.com.ar>
2017-01-08 17:56:23 +0000

    UrlsMemoryQueueTest::testInterleavedAddAndGetOrder()

--- Base/Tests/UrlsMemoryQueueTest.php
+++ Base/Tests/UrlsMemoryQueueTest.php
@@ -24,4 +24,18 @@ class UrlsMemoryQueueTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('url 1', $queue->getUrl());
         $this->assertNull($queue->getUrl());
     }
+    
+    public function testInterleavedAddAndGetOrder()
+    {
+        $queue = new UrlsMemoryQueue();
+
+        $queue->addUrl('url 1');
+        $queue->addUrl('url 2');
+        $queue->getUrl();
+        $queue->addUrl('url 3');
+
+        $this->assertEquals('url 3', $queue->getUrl());
+        $this->assertEquals('url 1', $queue->getUrl());
+        $this->assertNull($queue->getUrl());
+    }
 }
